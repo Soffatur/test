8b20524bc6cfa88c0a158cfcb353f59d0ea0e534
Soffatur <soffatur26@gmail.com>
2022-11-20 10:26:41 +0700

    test reviews product with params in controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,7 +7,72 @@ use Illuminate\Http\Request;
 class TestController extends Controller
 {
     public function product($id){
-        dd($id);
+        $products = json_decode(file_get_contents(public_path('json/products.json'), true));
+        $productCheck = false;
+        // check products
+        foreach($products as $product){
+            if($product->id == $id){
+                $productCheck = true;
+            }
+        }
+
+        $payload = ['status' => 'fail', 'message' => 'data not exists'];
+
+        if($productCheck == false){
+            return response()->json($payload, 404);
+        }
+        // end check products
+
+        $reviews = json_decode(file_get_contents(public_path('json/reviews.json'), true));
+        $star1 = 0;
+        $star2 = 0;
+        $star3 = 0;
+        $star4 = 0;
+        $star5 = 0;
+        $count = 0;
+
+        foreach($reviews as $review){
+            // check product_id
+            if($review->product_id == $id){
+                $count += 1;
+                // 1_star
+                if($review->rating == 1){
+                    $star1 += 1;
+                }
+                // 2_star
+                if($review->rating == 2){
+                    $star2 += 1;
+                }
+                // 3_star
+                if($review->rating == 3){
+                    $star3 += 1;
+                }
+                // 4_star
+                if($review->rating == 4){
+                    $star4 += 1;
+                }
+                // 5_star
+                if($review->rating == 5){
+                    $star5 += 1;
+                }
+            }
+        }
+
+        $average = ((1*$star1) + (2*$star2) + (3*$star3) + (4*$star4) + (5*$star5)) / ($star1+$star2+$star3+$star4+$star5);
+
+        $output = [
+            'total_reviews' => $count,
+            'average_ratings' => round($average, 1),
+            '5_star'          => $star5,
+            '4_star'          => $star4,
+            '3_star'          => $star3,
+            '2_star'          => $star2,
+            '1_star'          => $star1,
+        ];
+
+        $payload = ['status' => 'success', 'data' => $output];
+
+        return response()->json($payload, 200);
     }
 
     public function reviews(){
